2cc6d86b099dbbe677e84fd91847a41b757082a3
Ronald <ronaldkurouzo@gmail.com>
2024-05-16 09:41:24 -0300

    up 3

--- src/CarrinhoDeCompras.java
+++ src/CarrinhoDeCompras.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
 public class CarrinhoDeCompras {
     
     List<Item> items = new ArrayList<>();
